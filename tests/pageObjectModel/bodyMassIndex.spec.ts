cde3fe88898400c3d5193154847659b0c87318d8
Quyen Dang <quyendang@MacBook-Air-cua-Quyen.local>
2025-11-02 14:16:09 +0700

    Resolve merge conflict in BMI test

--- tests/pageObjectModel/bodyMassIndex.spec.ts
+++ tests/pageObjectModel/bodyMassIndex.spec.ts
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import { test, expect } from '@playwright/test';
 
 test('Body Mass Index (BMI) Calculator', () => {
@@ -10,3 +11,17 @@ test('Body Mass Index (BMI) Calculator', () => {
     await page.locator('#ckg').fill('70');
     await page.getByRole('button', { name: 'Calculate' }).click();
 });
+=======
+// import { test, expect } from '@playwright/test';
+
+// test('Body Mass Index (BMI) Calculator', () => {
+//     await page.goto('https://the-internet.herokuapp.com/login');
+//     await page.getByRole('link', { name: 'Metric Units' }).click();
+//     await page.locator('#cheightmeter').click();
+//     await page.locator('#cheightmeter').fill('170');
+
+//     await page.locator('#ckg').click();     
+//     await page.locator('#ckg').fill('70');
+//     await page.getByRole('button', { name: 'Calculate' }).click();
+// });
+>>>>>>> 4a8e68f (Initial Playwright project)
